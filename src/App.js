9b1c9430569f42c97c39a10bb73fb99addfbbbfb
Worawut Tititumjariya <woravootbat@gmail.com>
2019-03-08 17:38:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Test to Deploy sth Web APP to Heroku.
           </p>
           <a
             className="App-link"
